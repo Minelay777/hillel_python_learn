723d671de41b8d03975af48f943045568450ab99
Minelay777 <jobmihail@gmail.com>
2022-08-09 18:22:29 +0300

    применил команду рефакторинг кода

--- Task_1_Game/game_rps_ls.py
+++ Task_1_Game/game_rps_ls.py
@@ -1,4 +1,5 @@
 import random
+
 """
 - создаём список вариантов
 - создаём переменную со случайным значением взятым из списка вариантов
@@ -10,24 +11,23 @@ import random
         если да - игра повторяется,
         если нет - программа завершается.
 """
-variants: list = ['rock', 'paper', 'scissors', 'lizard', 'spok'] # список вариантов
-
+variants: list = ['rock', 'paper', 'scissors', 'lizard', 'spok']  # список вариантов
 
 while True:
     random_choice = random.choice(variants)  # случайный выбор машины
-    player_choice: str = input('Your choice (rock paper scissors lizard spok)? ')
+    player_choice: str = input('Your choice (rock paper scissors lizard spok)? ')  # просим игрока ввести свой выбор
 
     if player_choice in variants:
 
-        if player_choice == 'rock': # выбор игрока камень
-            if random_choice == 'rock': # при совпадении выбра - ничья
+        if player_choice == 'rock':  # выбор игрока камень
+            if random_choice == 'rock':  # при совпадении выбра - ничья
                 print('dead heat')
             elif random_choice == 'paper' or random_choice == 'spok':
                 print('You LOSE')
             elif random_choice == 'scissors' or random_choice == 'lizard':
                 print('You WIN')
 
-        if player_choice == 'paper': # выбор игрока бумага
+        if player_choice == 'paper':  # выбор игрока бумага
             if random_choice == 'paper':
                 print('dead heat')
             elif random_choice == 'rock' or random_choice == 'scissors':
@@ -35,7 +35,7 @@ while True:
             elif random_choice == 'lizard' or random_choice == 'spok':
                 print('You WIN')
 
-        if player_choice == 'scissors': # выбор игрока ножницы
+        if player_choice == 'scissors':  # выбор игрока ножницы
             if random_choice == 'scissors':
                 print('dead heat')
             elif random_choice == 'rock' or random_choice == 'spok':
@@ -43,7 +43,7 @@ while True:
             elif random_choice == 'paper' or random_choice == 'lizard':
                 print('You WIN')
 
-        if player_choice == 'lizard': # выбор игрока ящерица
+        if player_choice == 'lizard':  # выбор игрока ящерица
             if random_choice == 'lizard':
                 print('dead heat')
             elif random_choice == 'rock' or random_choice == 'scissors':
@@ -51,7 +51,7 @@ while True:
             elif random_choice == 'paper' or random_choice == 'spok':
                 print('You WIN')
 
-        if player_choice == 'spok': # выбор игрока Спок
+        if player_choice == 'spok':  # выбор игрока Спок
             if random_choice == 'spok':
                 print('dead head')
             elif random_choice == 'paper' or random_choice == 'lizard':
@@ -60,6 +60,7 @@ while True:
                 print('You WIN')
     else:
         print(f'Invalid input {player_choice}')  # если игрок ввёл некорректные данные выводим сообщение
-    player_continue_choice = input('Do you want to continue: y or n: ')  # спрашиваем, хочет ли игрок продолжить или закончить
+    player_continue_choice = input(
+        'Do you want to continue: y or n: ')  # спрашиваем, хочет ли игрок продолжить или закончить
     if player_continue_choice == 'n':
         break
